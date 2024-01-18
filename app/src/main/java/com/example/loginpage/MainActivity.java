a46cb4353e1fcde094dab0ecb2a3af3a0fbb4e11
Sandip040401 <iemsandip@gmail.com>
2024-01-18 16:05:59 +0530

    first Commit

---
+++ app/src/main/java/com/example/loginpage/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.loginpage;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.loginpage.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        initUI();
+    }
+
+    private void initUI() {
+        binding.fabBack.setOnClickListener(view -> {
+            // Implement your action for fabBack click here
+        });
+
+        binding.btnSignIn.setOnClickListener(view -> {
+            // Implement your action for btnSignIn click here
+        });
+
+        binding.tvForgotPassword.setOnClickListener(view -> {
+            // Implement your action for tvForgotPassword click here
+        });
+
+        binding.tvSignup.setOnClickListener(view -> {
+            // Implement your action for tvSignup click here
+        });
+    }
+}
